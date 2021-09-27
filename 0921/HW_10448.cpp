7b08c14a24642956e556dce56fe0858106b2f04c
Seong-Gyeong <63237389+Seong-Gyeong@users.noreply.github.com>
2021-09-27 12:32:52 +0900

    Update HW_10448.cpp

--- 0921/HW_10448.cpp
+++ 0921/HW_10448.cpp
@@ -8,13 +8,7 @@
 #include <vector>
 
 using namespace std;
-vector<int> v;
 
-void pop(vector<int> v1) {
-    for (int i = 0; i < v1.size(); i++) {
-        v1.pop_back();
-    }
-}
 
 //3개의 삼각수로 표현이 가능한지 판별하는 함수
 int triangular(int n) {
@@ -22,29 +16,27 @@ int triangular(int n) {
     //이렇게 하면 삼중반복문에서는 v.size()함수를 3번 돌려야 하니까
     //연산횟수 줄이기 위해서 변수에 v.size() 저장
 
-    for (int j = 1;; j++) {
+    vector<int> v(50);
+    int j;
+
+    for (j = 1;; j++) {
         int k = j * (j + 1) / 2;
-        if (k > n) break;
-        v.push_back(k);
+        if (k >= n) break;
+        v[j-1]=k;
     } //n보다 작거나 같은 삼각수들을 모두 벡터에 담는다
 
-    int s = v.size();
 
-    for (int i = 0; i < s; i++) {
-        for (int j = 0; j < s; j++) {
-            for (int t = 0; t < s; t++) {
-                if (v[i] + v[j] + v[t] == n) {
-                    pop(v);
+    for (int i = 0; i < j-1; i++) {
+        for (int r = 0; r < j-1; r++) {
+            for (int t = 0; t < j-1; t++) {
+                if (v[i] + v[r] + v[t] == n) {
                     return 1;
                 }
             }
         }
     }
 
-    pop(v);
     return 0;
-
-
 }
 
 int main() {
@@ -53,7 +45,7 @@ int main() {
 
     for (int i = 0; i < n; i++) {
         cin >> k;
-        cout << triangular(k);
+        cout << triangular(k)<<'\n';
     }
 
     return 0;
